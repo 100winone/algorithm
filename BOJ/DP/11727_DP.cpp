8fee15910da31fa4f0cd8d92c6008f8abf63f5ae
100winone <53801551+100winone@users.noreply.github.com>
2020-03-11 19:40:08 +0900

    DP

---
+++ BOJ/DP/11727_DP.cpp
@@ -0,0 +1,16 @@
+/*11727 DP 2xN 타일링2 20200311*/
+#include <stdio.h>
+using namespace std;
+int d[1001];
+int main(){
+    int n;
+    scanf("%d", &n);
+    d[0] = 1;
+    d[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        d[i] = d[i - 1] + d[i - 2] * 2;
+        d[i] %= 10007;
+    }
+    printf("%d\n", d[n]);
+}
+
