9732be9a0a2736c383ce384243a83886fb260f15
100winone <53801551+100winone@users.noreply.github.com>
2020-03-18 13:38:34 +0900

    BFS

--- boj/BFSDFS/2178_BFSDFS.cpp
+++ boj/BFSDFS/2178_BFSDFS.cpp
@@ -1,71 +1,47 @@
-/*
- * 2178 미로탐색 문제 (메모리 부족)
- * */
-
-#include <cstdio>
-#include <cstring>
+/*2178 미로탐색 BFS 20200318*/
+#include <iostream>
 #include <queue>
 #include <vector>
-#include <iostream>
-
 using namespace std;
-
-
-int M, N;
-bool check[101][101];
-int graph[101][101];
-int d[101][101];
-int dx[] = {-1, 1, 0, 0};
+int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, -1, 1};
-
-void init()
-{
-    //맵 초기화
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            graph[i][j] = 0;
-            check[i][j] = false;
-            d[i][j] = 0;
-        }
-    }
-}
-
-void bfs(int x, int y){
-    d[0][0] = 1;
-    check[0][0] = true;
-    queue<pair<int, int>> q;
-    q.push(make_pair(x, y));
+bool checked[101][101];
+char a[101][101];
+int dis[101][101];
+queue<pair<int, int>> q;
+void bfs(int n, int m){
     while(!q.empty()){
-        x = q.front().first;
-        y = q.front().second;
+        int x = q.front().first;
+        int y = q.front().second;
         q.pop();
-
-        for(int i = 0; i < 4; i++){
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
-
-            if(nextX >= 0 && nextX < N && nextY >= 0 && nextY < M){
-                if(graph[nextX][nextY] && !check[nextX][nextY]){
-                    check[nextX][nextY] == true;
-                    d[nextX][nextY] = d[x][y] + 1;
-                    q.push(make_pair(nextX, nextY));
+        for (int i = 0; i < 4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx >= 1 && nx <= n && ny >= 1 && ny <= m){
+                if(a[nx][ny] == '1' && !checked[nx][ny]) {
+                    q.push(make_pair(nx, ny));
+                    dis[nx][ny] = dis[x][y] + 1;
+                    checked[nx][ny] = true;
                 }
             }
         }
     }
+    cout << dis[n][m] << '\n';
+//    printf("%d\n", dis[n][m]);
 }
 int main() {
-    init();
-    scanf("%d %d", &N, &M);
-    for(int i = 0; i < N; i++){
-        for (int j = 0; j < M; ++j) {
-            scanf("%1d", &graph[i][j]);
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            cin >> a[i][j];
         }
     }
-
-    bfs(0, 0);
-    printf("%d\n", d[N - 1][M - 1]);
-    return 0;
+    q.push(make_pair(1, 1));
+    checked[1][1] = true;
+    dis[1][1] = 1;
+    bfs(n, m);
 }
