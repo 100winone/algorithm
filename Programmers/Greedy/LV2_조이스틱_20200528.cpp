90c0b53b71ca4ffe53c4b04e16e55f020f19f867
100winone <53801551+100winone@users.noreply.github.com>
2020-05-31 23:48:52 +0900

    Create LV2_조이스틱_20200528.cpp

---
+++ Programmers/Greedy/LV2_조이스틱_20200528.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(string name) {
+    int answer = 0;
+    int size = name.size();
+    
+    return answer;
+}
+int main(){
+    cout << solution("JEROEN") << '\n';
+    return 0;
+}
