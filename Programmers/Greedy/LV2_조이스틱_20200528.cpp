d696b52795d14b317ae3afc793acd2a8875bf2d6
100winone <53801551+100winone@users.noreply.github.com>
2020-06-01 14:41:27 +0900

    Greedy

--- Programmers/Greedy/LV2_조이스틱_20200528.cpp
+++ Programmers/Greedy/LV2_조이스틱_20200528.cpp
@@ -1,15 +1,34 @@
 #include <string>
-#include <vector>
 #include <iostream>
 using namespace std;
-
 int solution(string name) {
     int answer = 0;
     int size = name.size();
-    
+    string checkName = "";
+    for (int i = 0; i < size; ++i) {
+        checkName += "A";
+    }
+    int cur = 0;
+    while(name != checkName){
+        int left = 0, right = 0, minIdx = 0, minCnt = size;
+        for (int i = 0; i < size; ++i) {
+            if(name[i] == checkName[i]) continue;
+            if(cur > i){
+                left = cur - i;
+                right = size - cur + i;
+            } else {
+                left = cur + size - i;
+                right = i - cur;
+            }
+            if(left < minCnt || right < minCnt){
+                minCnt = min(left, right);
+                minIdx = i;
+            }
+        }
+        cur = minIdx;
+        answer += minCnt;
+        answer += min(name[cur] - 'A', 'Z' - name[cur] + 1);
+        checkName[cur] = name[cur];
+    }
     return answer;
-}
-int main(){
-    cout << solution("JEROEN") << '\n';
-    return 0;
 }
