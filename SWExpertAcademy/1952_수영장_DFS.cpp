97465dcdd86785b44b18d3f1a44ee0a1886e2107
100winone <53801551+100winone@users.noreply.github.com>
2020-06-25 14:31:31 +0900

    Update 1952_수영장_DFS.cpp

--- SWExpertAcademy/1952_수영장_DFS.cpp
+++ SWExpertAcademy/1952_수영장_DFS.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
-
 
 int main(){
     int t;
     cin >> t;
 
-    for (int i = 0; i < t; ++i) {
-        int price[4];
-        int month[13];
-        int totalPrice = -987654321;
-        int sum = 0;
+    for (int i = 1; i <= t; ++i) {
+        int price[4] = {0, };
+        int month[13] = {0, }; // 사용하는 날짜 갯수
+        int diffMonthAndDay[13] = {0, }; // 일로써 할 것인지 달로써 할 것인지
+        int answer[13] = {0, };
         for (int j = 0; j < 4; ++j) {
-            cin >> price[i];
+            cin >> price[j];
         }
-        for (int j = 0; j < 12; ++j) {
+        for (int j = 1; j <= 12; ++j) {
             cin >> month[j];
         }
-        int dayMonth = price[1] / price[0] + 1;
-
-
+        for (int j = 1; j <= 12; ++j) {
+            diffMonthAndDay[j] = min((price[0] * month[j]), price[1]);
+        }
+        for (int j = 1; j <= 12; ++j) {
+            answer[j] = answer[j - 1] + diffMonthAndDay[j];
+            if(j >= 3) {
+                if(answer[j] > answer[j - 3] + price[2]){
+                    answer[j] = answer[j - 3] + price[2];
+                }
+            }
+        }
+        /* answer[12]랑 1년 값이랑 비교 */
+        if(answer[12] > price[3]) answer[12] = price[3];
+        cout << '#' << i << ' ' << answer[12] << '\n';
     }
+
     return 0;
 }
